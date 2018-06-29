e6da75dd081c146b89bb0053dfe5ea2b2d6c8c75
Tangeni Shikomba <tangenishikomba@gmail.com>
2018-06-29 12:33:34 +0200

    admin guard created

---
+++ src/app/guards/roles/is-admin.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsAdminGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  canActivate() {
+    if (!AuthService.isAdmin()) {
+      return true;
+    }
+  }
+}

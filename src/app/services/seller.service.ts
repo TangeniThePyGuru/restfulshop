0d60fc771ae0a3c167c881de65310a9043308795
Tangeni Shikomba <tangenishikomba@gmail.com>
2018-07-09 17:24:34 +0200

    Seller Service Completed

---
+++ src/app/services/seller.service.ts
@@ -0,0 +1,134 @@
+import { Injectable } from '@angular/core';
+import {AuthService} from './auth.service';
+import {Headers, Http, RequestOptions} from '@angular/http';
+import {CONFIG} from '../config/config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SellerService {
+
+  private readonly headers: Headers;
+  private resourceBaseURL = `${CONFIG.API_URL}/sellers`;
+
+  constructor(
+    private authService: AuthService,
+    private http: Http,
+  ) {
+    this.headers = new Headers({'Authorization': `Bearer ${this.authService.getToken()}`});
+  }
+
+  get(endPoint = null): Promise<any> {
+
+    let url ;
+    if (endPoint) {
+      url  = endPoint;
+    } else {
+      url = `${this.resourceBaseURL}}`;
+    }
+    const option = new RequestOptions({ headers: this.headers});
+
+    return this.http.get(url, option )
+      .toPromise()
+      .then(response => {
+        return response.json();
+      });
+
+  }
+
+  getById(sellerId: number): Promise<any> {
+    const url = `${this.resourceBaseURL}/${sellerId}`;
+    const option = new RequestOptions({ headers: this.headers});
+
+    return this.http.get(url, option )
+      .toPromise()
+      .then(response => {
+        // console.log(response);
+        return response.json();
+      });
+  }
+
+  getSellerTransactions(sellerId: number): Promise<any> {
+    const url = `${this.resourceBaseURL}/${sellerId}/transactions/`;
+    const option = new RequestOptions({ headers: this.headers});
+
+    return this.http.get(url, option )
+      .toPromise()
+      .then(response => {
+        // console.log(response);
+        return response.json();
+      });
+  }
+
+  getSellerBuyers(sellerId: number): Promise<any> {
+    const url = `${this.resourceBaseURL}/${sellerId}/buyers/`;
+    const option = new RequestOptions({ headers: this.headers});
+
+    return this.http.get(url, option )
+      .toPromise()
+      .then(response => {
+        // console.log(response);
+        return response.json();
+      });
+  }
+
+  getSellerCategories(sellerId: number): Promise<any> {
+    const url = `${this.resourceBaseURL}/${sellerId}/categories`;
+    const option = new RequestOptions({ headers: this.headers});
+
+    return this.http.get(url, option )
+      .toPromise()
+      .then(response => {
+        // console.log(response);
+        return response.json();
+      });
+  }
+
+  getSellerProducts(sellerId: number): Promise<any> {
+    const url = `${this.resourceBaseURL}/${sellerId}/products/`;
+    const option = new RequestOptions({ headers: this.headers});
+
+    return this.http.get(url, option )
+      .toPromise()
+      .then(response => {
+        // console.log(response);
+        return response.json();
+      });
+  }
+
+  storeSellerProduct(sellerId: number, product: any ): Promise<any> {
+    const url = `${this.resourceBaseURL}/${sellerId}/products/`;
+    const option = new RequestOptions({ headers: this.headers});
+
+    return this.http.post(url, product, option )
+      .toPromise()
+      .then(response => {
+        // console.log(response);
+        return response.json();
+      });
+  }
+
+  updateSellerProduct(sellerId: number, productId: number, product: any ): Promise<any> {
+    const url = `${this.resourceBaseURL}/${sellerId}/products/${productId}`;
+    const option = new RequestOptions({ headers: this.headers});
+
+    return this.http.put(url, product, option )
+      .toPromise()
+      .then(response => {
+        // console.log(response);
+        return response.json();
+      });
+  }
+
+  destroySellerProduct(sellerId: number, productId: number): Promise<any> {
+    const url = `${this.resourceBaseURL}/${sellerId}/products/${productId}`;
+    const option = new RequestOptions({ headers: this.headers});
+
+    return this.http.delete(url, option )
+      .toPromise()
+      .then(response => {
+        // console.log(response);
+        return response.json();
+      });
+  }
+}

e7f5c7539d0fb8acbfc6fe0e0283474119c5d930
Tangeni Shikomba <tangenishikomba@gmail.com>
2018-07-09 20:12:51 +0200

    routes updated

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,9 +3,15 @@ import {AuthedGuard} from '../guards/authed/authed.guard';
 import {DashboardComponent} from '../dashboard/dashboard.component';
 import {AuthGuard} from '../guards/auth/auth.guard';
 import {HomeComponent} from '../home/home.component';
+import {BuyersComponent} from '../users/buyer/buyers/buyers.component';
+import {SellersComponent} from '../users/seller/sellers/sellers.component';
+import {UsersTemplateComponent} from '../templates/users-template/users-template.component';
 
 export const ROUTES = [
   { path: 'auth/login', component: LoginComponent, canActivate: [AuthedGuard]},
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  { path: 'users', component: UsersTemplateComponent, canActivate: [AuthGuard],
+          children: [{path: 'buyers', component: BuyersComponent },
+                     {path: 'sellers', component: SellersComponent }]},
   { path: '', component: HomeComponent, canActivate: [AuthedGuard]},
 ];

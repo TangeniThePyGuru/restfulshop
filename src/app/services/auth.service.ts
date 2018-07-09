780bbffed23730b80243d8e8b527f88d56f0588a
Tangeni Shikomba <tangenishikomba@gmail.com>
2018-07-09 23:48:22 +0200

    auth service updated, comments added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -21,23 +21,38 @@ export class AuthService {
 
   }
 
+  /**
+   *
+   * @returns {AuthToken}
+   */
   getToken(): AuthToken {
     return JSON.parse(localStorage.getItem('token'));
   }
 
+  /**
+   *
+   * @returns {User}
+   */
   getAuthUser(): User {
     return JSON.parse(localStorage.getItem('user'));
   }
 
+  /**
+   *
+   * @returns {boolean}
+   */
   isAdmin(): boolean {
     return this.getAuthUser().admin;
   }
 
+  /**
+   *
+   * @returns {boolean}
+   */
   isLoggedIn(): boolean {
     const token = this.getToken() ? this.getToken().access_token : null;
     const user =  this.getAuthUser();
-    const expires_in = this.getToken() ? this.getToken().expires_in : null;
-    // console.log(expires_in);
+    // const expires_in = this.getToken() ? this.getToken().expires_in : null;
     // user is authenticated
     if ( token && user) {
       return true;
@@ -46,10 +61,21 @@ export class AuthService {
     return false;
   }
 
+  /**
+   *
+   * @returns {number}
+   */
   getAuthUserId(): number {
     return this.getAuthUser().id;
   }
 
+  /**
+   *
+   * @param {string} name
+   * @param {string} email
+   * @param {string} password
+   * @returns {Promise<UserData>}
+   */
   register(name: string, email: string, password: string): Promise<UserData> {
     return this.http.post(`${CONFIG.API_URL}/register`, { name: name, email: email, password: password })
       .toPromise()
@@ -64,6 +90,12 @@ export class AuthService {
       });
   }
 
+  /**
+   *
+   * @param {string} email
+   * @param {string} password
+   * @returns {Promise<UserData>}
+   */
   login(email: string, password: string): Promise<UserData> {
     return this.http.post(`${CONFIG.API_URL}/authenticate`,
       { username: email, password: password })
@@ -75,7 +107,8 @@ export class AuthService {
                                     response.json().auth.access_token,
                                     response.json().auth.refresh_token) ;
         const user = new User(response.json().user.id, response.json().user.name,
-                              response.json().user.email, response.json().user.admin);
+                              response.json().user.email, response.json().user.admin,
+                              response.json().user.created_at, response.json().user.updated_at);
 
         console.log(token, user);
         // const
@@ -86,9 +119,12 @@ export class AuthService {
       .catch(error => {
         return Promise.reject(error.json());
       });
-
   }
 
+  /**
+   *
+   * @param {UserData} userData
+   */
   logUserIn(userData: UserData): void {
     localStorage.setItem('token', JSON.stringify(userData.token));
 
